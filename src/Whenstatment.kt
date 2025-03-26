8dd3cfb04ec771f517d19c40c2ce62016d475701
VEXALIA <ruoroabigael@gmail.com>
2025-03-26 14:29:40 +0300

    Add files via upload

---
+++ src/Whenstatment.kt
@@ -0,0 +1,23 @@
+import java.util.Scanner
+fun main() {
+    var read = Scanner(System.`in`)
+    println("Enter Day(number) :")
+    var day = read.nextInt()
+
+
+    var result = when(day){
+        1 -> "Monday"
+        2 -> "Tuesday"
+        3 -> "Wednsday"
+        4 -> "Thursday"
+        5 -> "Friday"
+        6 -> "Saturday"
+        7 -> "Sunday"
+        else ->"Invalid day of the week"
+    }
+    println("Day $day corresponds to $result")
+
+}
+
+
+
